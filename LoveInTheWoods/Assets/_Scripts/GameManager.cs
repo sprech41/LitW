dced64fefdf1e70d6fb98489dd7a664ee133252b
Darius <y.ziener@gmx.de>
2017-10-15 01:26:48 +0200

    Added the GameManager that doesn't get deleted when scenes are changed.

---
+++ LoveInTheWoods/Assets/_Scripts/GameManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+    public static GameManager instance;
+
+    public bool maeAngery;  //possible public variables accesible all over the game
+
+
+
+
+
+    //here start the functions that create the DontDestroyOnLoad gameObject and result in this class being dragged through all scenes
+    void Awake()
+    {
+        MakeSingleton();
+    }
+
+    private void MakeSingleton()
+    {
+        if (instance != null) {
+            Destroy(gameObject);
+        }
+        else  {instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+   }
+}
