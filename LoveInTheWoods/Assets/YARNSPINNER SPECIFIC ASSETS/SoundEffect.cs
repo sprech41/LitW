9d785f97cc4aea557c43c0c241281e99fcd3dfe0
sprech41 <sprech41@gmail.com>
2017-11-15 19:38:53 -0500

    Added Sound Effects
    Added function and yarn command to play sound effects.

---
+++ LoveInTheWoods/Assets/YARNSPINNER SPECIFIC ASSETS/SoundEffect.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+namespace Yarn.Unity.Example{
+	public class SoundEffect : MonoBehaviour {
+		public AudioClip[] clips;
+		private AudioSource source;
+
+		// Use this for initialization
+		void Start () {
+			source = GetComponent<AudioSource> ();
+		}
+	
+		[YarnCommand("playsound")]
+		public void playSound (string n)
+		{
+			AudioClip clip = null;
+			foreach (AudioClip c in clips) {
+				if (c.name == n){
+					clip = c;
+					break;
+				}
+			}
+
+			if (clip == null)
+			{
+				Debug.LogFormat ("Clip of name {0} not found", n);
+				return;
+			}
+
+			source.PlayOneShot (clip,1F);
+		}
+	}
+}
